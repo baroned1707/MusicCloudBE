e2cb9ffddc05891735a24a8d84f57aef46fdb207
baroned1707 <nguyenduykhuongtqtpy@gmail.com>
2021-08-03 13:55:02 +0700

    update

--- config/selenium.js
+++ config/selenium.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const webdriver = require("selenium-webdriver");
-const { Builder } = require("selenium-webdriver");
-const edge = require("selenium-webdriver/edge");
-const pathEdgeDriver = path.join(__dirname.replace("\\config", "") + "\\driver") + "\\msedgedriver.exe";
-const service = new edge.ServiceBuilder(pathEdgeDriver);
-const edegOption = new edge.Options();
-edegOption.addArguments("headless");
+// const path = require("path");
+// const webdriver = require("selenium-webdriver");
+// const { Builder } = require("selenium-webdriver");
+// const edge = require("selenium-webdriver/edge");
+// const pathEdgeDriver = path.join(__dirname.replace("\\config", "") + "\\driver") + "\\msedgedriver.exe";
+// const service = new edge.ServiceBuilder(pathEdgeDriver);
+// const edegOption = new edge.Options();
+// edegOption.addArguments("headless");
 
-const driverEdge = new Builder().forBrowser("MicrosoftEdge").setEdgeService(service).setEdgeOptions(edegOption).build();
+// const driverEdge = new Builder().forBrowser("MicrosoftEdge").setEdgeService(service).setEdgeOptions(edegOption).build();
 
 //chorme
 
